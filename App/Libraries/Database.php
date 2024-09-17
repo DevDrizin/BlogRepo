93d41f6084ab4af408c9628bbdebf622864656c1
drizinn <adrianomoura69942@gmail.com>
2024-09-17 14:30:31 -0400

    banco de dados

---
+++ App/Libraries/Database.php
@@ -0,0 +1,10 @@
+<?class Database {
+    private $host = 'localhost;
+    private $usurio = 'root';
+    private $senha = '';
+    private $banco = 'blog';
+    private $porta = '3306'
+
+    private $dbh;
+
+}
